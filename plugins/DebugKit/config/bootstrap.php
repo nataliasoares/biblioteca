03f486c156b8a9812d73822f8d97b2cfb10edadc
nataliasoares <38406843+nataliasoares@users.noreply.github.com>
2018-04-16 13:00:20 -0300

    alteracao

--- plugins/DebugKit/config/bootstrap.php
+++ plugins/DebugKit/config/bootstrap.php
@@ -1,216 +1,103 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.8
- * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
-/*
- * Configure paths required to find CakePHP + general filepath constants
- */
-require __DIR__ . '/paths.php';
-
-/*
- * Bootstrap CakePHP.
- *
- * Does the various bits of setup that CakePHP needs to do.
- * This includes:
- *
- * - Registering the CakePHP autoloader.
- * - Setting the default application paths.
- */
-require CORE_PATH . 'config' . DS . 'bootstrap.php';
-
-use Cake\Cache\Cache;
-use Cake\Console\ConsoleErrorHandler;
-use Cake\Core\App;
 use Cake\Core\Configure;
-use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Core\Plugin;
-use Cake\Database\Type;
 use Cake\Datasource\ConnectionManager;
-use Cake\Error\ErrorHandler;
-use Cake\Http\ServerRequest;
+use Cake\Event\EventManager;
 use Cake\Log\Log;
-use Cake\Mailer\Email;
-use Cake\Utility\Inflector;
-use Cake\Utility\Security;
+use Cake\ORM\Query;
+use Cake\Routing\DispatcherFactory;
+use DebugKit\DebugSql;
+use DebugKit\Middleware\DebugKitMiddleware;
+use DebugKit\Routing\Filter\DebugBarFilter;
+use DebugKit\ToolbarService;
 
-/**
- * Uncomment block of code below if you want to use `.env` file during development.
- * You should copy `config/.env.default to `config/.env` and set/modify the
- * variables as required.
- */
-// if (!env('APP_NAME') && file_exists(CONFIG . '.env')) {
-//     $dotenv = new \josegonzalez\Dotenv\Loader([CONFIG . '.env']);
-//     $dotenv->parse()
-//         ->putenv()
-//         ->toEnv()
-//         ->toServer();
-// }
+$service = new ToolbarService(EventManager::instance(), (array)Configure::read('DebugKit'));
 
-/*
- * Read configuration file and inject configuration into various
- * CakePHP classes.
- *
- * By default there is only one configuration file. It is often a good
- * idea to create multiple configuration files, and separate the configuration
- * that changes from configuration that does not. This makes deployment simpler.
- */
-try {
-    Configure::config('default', new PhpConfig());
-    Configure::load('app', 'default', false);
-} catch (\Exception $e) {
-    exit($e->getMessage() . "\n");
+if (!$service->isEnabled() || php_sapi_name() === 'cli' || php_sapi_name() === 'phpdbg') {
+    return;
 }
 
-/*
- * Load an environment local configuration file.
- * You can use a file like app_local.php to provide local overrides to your
- * shared configuration.
- */
-//Configure::load('app_local', 'default');
-
-/*
- * When debug = true the metadata cache should only last
- * for a short time.
- */
-if (Configure::read('debug')) {
-    Configure::write('Cache._cake_model_.duration', '+2 minutes');
-    Configure::write('Cache._cake_core_.duration', '+2 minutes');
+$hasDebugKitConfig = ConnectionManager::getConfig('debug_kit');
+if (!$hasDebugKitConfig && !in_array('Postgres', PDO::getAvailableDrivers())) {
+    $msg = 'DebugKit not enabled. You need to either install pdo_sqlite, ' .
+        'or define the "debug_kit" connection name.';
+    Log::warning($msg);
+    return;
 }
 
-/*
- * Set server timezone to UTC. You can change it to another timezone of your
- * choice but using UTC makes time calculations / conversions easier.
- * Check http://php.net/manual/en/timezones.php for list of valid timezone strings.
- */
-date_default_timezone_set('UTC');
+if (!$hasDebugKitConfig) {
+    ConnectionManager::setConfig('debug_kit', [
+        'className' => 'Cake\Database\Connection',
+        'driver' => 'Cake\Database\Driver\Sqlite',
+        'database' => TMP . 'debug_kit.sqlite',
+        'encoding' => 'utf8',
+        'cacheMetadata' => true,
+        'quoteIdentifiers' => false,
+    ]);
+}
 
-/*
- * Configure the mbstring extension to use the correct encoding.
- */
-mb_internal_encoding(Configure::read('App.encoding'));
+if (Plugin::routes('DebugKit') === false) {
+    require __DIR__ . DS . 'routes.php';
+}
 
-/*
- * Set the default locale. This controls how dates, number and currency is
- * formatted and sets the default language to use for translations.
- */
-ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
-
-/*
- * Register application error and exception handlers.
- */
-$isCli = PHP_SAPI === 'cli';
-if ($isCli) {
-    (new ConsoleErrorHandler(Configure::read('Error')))->register();
+$appClass = Configure::read('App.namespace') . '\Application';
+if (class_exists($appClass)) {
+    EventManager::instance()->on('Server.buildMiddleware', function ($event, $queue) use ($service) {
+        $middleware = new DebugKitMiddleware($service);
+        $queue->insertAt(0, $middleware);
+    });
 } else {
-    (new ErrorHandler(Configure::read('Error')))->register();
+    // Setup dispatch filter
+    $debugBar = new DebugBarFilter(EventManager::instance(), (array)Configure::read('DebugKit'));
+    $debugBar->setup();
+    DispatcherFactory::add($debugBar);
 }
 
-/*
- * Include the CLI bootstrap overrides.
- */
-if ($isCli) {
-    require __DIR__ . '/bootstrap_cli.php';
-}
-
-/*
- * Set the full base URL.
- * This URL is used as the base of all absolute links.
- *
- * If you define fullBaseUrl in your config file you can remove this.
- */
-if (!Configure::read('App.fullBaseUrl')) {
-    $s = null;
-    if (env('HTTPS')) {
-        $s = 's';
+if (!function_exists('sql')) {
+    /**
+     * Prints out the SQL statements generated by a Query object.
+     *
+     * This function returns the same variable that was passed.
+     * Only runs if debug mode is enabled.
+     *
+     * @param Query $query Query to show SQL statements for.
+     * @param bool $showValues Renders the SQL statement with bound variables.
+     * @param bool|null $showHtml If set to true, the method prints the debug
+     *    data in a browser-friendly way.
+     * @return Query
+     */
+    function sql(Query $query, $showValues = true, $showHtml = null)
+    {
+        return DebugSql::sql($query, $showValues, $showHtml, 1);
     }
-
-    $httpHost = env('HTTP_HOST');
-    if (isset($httpHost)) {
-        Configure::write('App.fullBaseUrl', 'http' . $s . '://' . $httpHost);
-    }
-    unset($httpHost, $s);
 }
 
-Cache::setConfig(Configure::consume('Cache'));
-ConnectionManager::setConfig(Configure::consume('Datasources'));
-Email::setConfigTransport(Configure::consume('EmailTransport'));
-Email::setConfig(Configure::consume('Email'));
-Log::setConfig(Configure::consume('Log'));
-Security::setSalt(Configure::consume('Security.salt'));
-
-/*
- * The default crypto extension in 3.0 is OpenSSL.
- * If you are migrating from 2.x uncomment this code to
- * use a more compatible Mcrypt based implementation
- */
-//Security::engine(new \Cake\Utility\Crypto\Mcrypt());
-
-/*
- * Setup detectors for mobile and tablet.
- */
-ServerRequest::addDetector('mobile', function ($request) {
-    $detector = new \Detection\MobileDetect();
-
-    return $detector->isMobile();
-});
-ServerRequest::addDetector('tablet', function ($request) {
-    $detector = new \Detection\MobileDetect();
-
-    return $detector->isTablet();
-});
-
-/*
- * Enable immutable time objects in the ORM.
- *
- * You can enable default locale format parsing by adding calls
- * to `useLocaleParser()`. This enables the automatic conversion of
- * locale specific date formats. For details see
- * @link https://book.cakephp.org/3.0/en/core-libraries/internationalization-and-localization.html#parsing-localized-datetime-data
- */
-Type::build('time')
-    ->useImmutable();
-Type::build('date')
-    ->useImmutable();
-Type::build('datetime')
-    ->useImmutable();
-Type::build('timestamp')
-    ->useImmutable();
-
-/*
- * Custom Inflector rules, can be set to correctly pluralize or singularize
- * table, model, controller names or whatever other string is passed to the
- * inflection functions.
- */
-//Inflector::rules('plural', ['/^(inflect)or$/i' => '\1ables']);
-//Inflector::rules('irregular', ['red' => 'redlings']);
-//Inflector::rules('uninflected', ['dontinflectme']);
-//Inflector::rules('transliteration', ['/å/' => 'aa']);
-
-/*
- * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
- * Uncomment one of the lines below, as you need. make sure you read the documentation on Plugin to use more
- * advanced ways of loading plugins
- *
- * Plugin::loadAll(); // Loads all plugins at once
- * Plugin::load('Migrations'); //Loads a single plugin named Migrations
- *
- */
-
-/*
- * Only try to load DebugKit in development mode
- * Debug Kit should not be installed on a production system
- */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
+if (!function_exists('sqld')) {
+    /**
+     * Prints out the SQL statements generated by a Query object and dies.
+     *
+     * Only runs if debug mode is enabled.
+     * It will otherwise just continue code execution and ignore this function.
+     *
+     * @param Query $query Query to show SQL statements for.
+     * @param bool $showValues Renders the SQL statement with bound variables.
+     * @param bool|null $showHtml If set to true, the method prints the debug
+     *    data in a browser-friendly way.
+     * @return void
+     */
+    function sqld(Query $query, $showValues = true, $showHtml = null)
+    {
+        DebugSql::sqld($query, $showValues, $showHtml, 2);
+    }
 }
